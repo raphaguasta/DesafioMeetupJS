0e31f1961a534f6afae7b259451056222cad1386
Raphael Guastaferro <rapha.guasta@gmail.com>
2016-12-02 14:18:40 -0200

    Utilizando GPU

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 Array.from(document.querySelectorAll('.box')).forEach(el => {
-    el.onclick = () => el.style.height = 0;
+    el.onclick = () => {
+        el.style.height = 0;
+        el.style.opacity = 0.9;
+    };
     el.addEventListener('transitionend', () => el.remove())
 });
